7d87cb7a551061ea178a5b5d4d6ccac8e6000812
gmoo <gerah@example.com>
2020-04-16 23:32:38 -0400

    first commit

---
+++ js/cust-script.js
@@ -0,0 +1,30 @@
+
+function alertNotice() {
+var fnm = document.forms["consultation"]["fname"].value;
+var lnm = document.forms["consultation"]["lname"].value;
+var phn = document.forms["consultation"]["phone"].value;
+var msg = document.forms["consultation"]["description"].value;
+  if (fnm == "") {
+  document.getElementById("err").style.display = "block";
+    document.getElementById("err").innerHTML = "First Name must be filled out"
+    return false;
+  }else if(lnm == "") {
+  document.getElementById("err").style.display = "block";
+    document.getElementById("err").innerHTML = "Last Name must be filled out"
+    return false;
+  } else if(phn == "") {
+  document.getElementById("err").style.display = "block";
+    document.getElementById("err").innerHTML = "Phone number must be filled out"
+    return false;
+  } else if(msg == "") {
+  document.getElementById("err").style.display = "block";
+    document.getElementById("err").innerHTML = "Description is required"
+    return false;
+  }  else {
+document.getElementById("err").style.display = "none";
+document.getElementById("notice").style.display = "block";
+}
+ 
+}
+
+// slider
