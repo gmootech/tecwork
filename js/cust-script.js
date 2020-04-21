fb05e8e8d96c7926548cbcf215a18ab492672e12
gmoo <gerah@example.com>
2020-04-20 20:10:59 -0400

    fix for contact us

--- js/cust-script.js
+++ js/cust-script.js
@@ -1,4 +1,3 @@
-
 function alertNotice() {
 var fnm = document.forms["consultation"]["fname"].value;
 var lnm = document.forms["consultation"]["lname"].value;
@@ -6,19 +5,19 @@ var phn = document.forms["consultation"]["phone"].value;
 var msg = document.forms["consultation"]["description"].value;
   if (fnm == "") {
   document.getElementById("err").style.display = "block";
-    document.getElementById("err").innerHTML = "First Name must be filled out"
+    document.getElementById("err").innerHTML = "First Name must be filled out";
     return false;
   }else if(lnm == "") {
   document.getElementById("err").style.display = "block";
-    document.getElementById("err").innerHTML = "Last Name must be filled out"
+    document.getElementById("err").innerHTML = "Last Name must be filled out";
     return false;
   } else if(phn == "") {
   document.getElementById("err").style.display = "block";
-    document.getElementById("err").innerHTML = "Phone number must be filled out"
+    document.getElementById("err").innerHTML = "Phone number must be filled out";
     return false;
   } else if(msg == "") {
   document.getElementById("err").style.display = "block";
-    document.getElementById("err").innerHTML = "Description is required"
+    document.getElementById("err").innerHTML = "Description is required";
     return false;
   }  else {
 document.getElementById("err").style.display = "none";
@@ -27,4 +26,9 @@ document.getElementById("notice").style.display = "block";
  
 }
 
-// slider
+// alert
+function messageStatus(){
+	var notice = document.getElementById('sendStatus');
+		notice.style.display = "inline-block";
+		notice.innerHTML ="Thank you for your message ! Someone will be in touch shortly.";
+}
